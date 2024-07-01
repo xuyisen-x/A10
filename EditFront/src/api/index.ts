795c9458e4355339144b8a9f55de33fc287406d6
xuyisen <xuyisen123456@outlook.com>
2024-07-01 21:22:17 +0800

    better editor

--- EditFront/src/api/index.ts
+++ EditFront/src/api/index.ts
@@ -10,3 +10,51 @@ export function adduser(username: string, password: string) {
         data: formData
     })
 }
+
+export function getPolish(user:string, key: string, cont: string){
+    let formData = new FormData();
+    formData.append("user", user);
+    formData.append("key", key);
+    formData.append("cont", cont);
+    return http.request({
+        url: '/getpolish',
+        method: 'post',
+        data: formData
+    })
+}
+
+export function getAbbreviate(user:string, key: string, cont: string){
+    let formData = new FormData();
+    formData.append("user", user);
+    formData.append("key", key);
+    formData.append("cont", cont);
+    return http.request({
+        url: '/getabbreviate',
+        method: 'post',
+        data: formData
+    })
+}
+
+export function getExpand(user:string, key: string, cont: string){
+    let formData = new FormData();
+    formData.append("user", user);
+    formData.append("key", key);
+    formData.append("cont", cont);
+    return http.request({
+        url: '/getexpand',
+        method: 'post',
+        data: formData
+    })
+}
+
+export function getExtend(user:string, key: string, cont: string){
+    let formData = new FormData();
+    formData.append("user", user);
+    formData.append("key", key);
+    formData.append("cont", cont);
+    return http.request({
+        url: '/getextend',
+        method: 'post',
+        data: formData
+    })
+}
